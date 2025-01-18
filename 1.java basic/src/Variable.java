c3fc1255d6f7fddcb9db0b77af6a1b4cfb5f8a75
Zainal <zainalfadliansyah@gmail.com>
2025-01-18 23:37:26 +0700

    Variable

---
+++ 1.java basic/src/Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        String name;
+        name = "Zainal Arifin Fadliansyah";
+
+        System.out.println(name);
+
+        int age = 24;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "kwon";
+        System.out.println(name);
+
+        var firstName = "Zainal";
+        var middleName = "Arifin";
+        var lastName = "Fadliansyah";
+
+        final String application = "Belajar Java";
+
+    }
+}
