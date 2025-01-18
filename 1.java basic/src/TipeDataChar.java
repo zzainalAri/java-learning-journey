bbe049da4a472d24eb2cf8d3dc9af3abce088ec7
Zainal <zainalfadliansyah@gmail.com>
2025-01-18 23:31:55 +0700

    data type char

---
+++ 1.java basic/src/TipeDataChar.java
@@ -0,0 +1,19 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+
+
+        char z = 'Z';
+        char a = 'A';
+        char i = 'I';
+        char n = 'N';
+        char L = 'L';
+
+        System.out.print(z);
+        System.out.print(a);
+        System.out.print(i);
+        System.out.print(n);
+        System.out.print(a);
+        System.out.print(L);
+
+    }
+}
