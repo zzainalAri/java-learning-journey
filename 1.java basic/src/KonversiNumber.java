5e9accc2cf58fab75a8967fe729f721191aa3544
Zainal <zainalfadliansyah@gmail.com>
2025-01-19 23:21:53 +0700

    Exercie

---
+++ 1.java basic/src/KonversiNumber.java
@@ -0,0 +1,14 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+        long iniLong = iniInt;
+
+        int iniInt2 = 10000;
+        byte iniByte2 =(byte) iniInt2;
+
+
+    }
+}
