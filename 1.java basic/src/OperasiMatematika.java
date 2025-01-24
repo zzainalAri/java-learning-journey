af496a00b4b43d3120d37cbf8531d31f90835674
Zainal <zainalfadliansyah@gmail.com>
2025-01-24 22:53:43 +0700

    Math Operation

---
+++ 1.java basic/src/OperasiMatematika.java
@@ -0,0 +1,35 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 90;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        int c = 12;
+
+        c += 10;
+        System.out.println(c);
+
+        c -= 2;
+        System.out.println(c);
+
+        c *= 12;
+        System.out.println(c);
+
+        int d = 120;
+        int e = -120;
+
+        d++;
+        System.out.println(d);
+
+        d--;
+        System.out.println(d);
+
+        System.out.println(!false);
+    }
+}
